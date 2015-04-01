4f624b58acd57034f9b16b4a7c34f9739b6f72b9
ryuyama <ryu.yamada.private@gmail.com>
2015-04-01 22:20:54 +0900

    Make each page titles displayed dynamically

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_title_is "Home"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_title_is "Help"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_title_is "About"
   end
+
+  private
+
+    def assert_title_is(name)
+      assert_select "title", "#{name} | Ruby on Rails Tutorial Sample App"
+    end
 end
