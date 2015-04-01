fa6511aa4c5344a46109333d10e837d8bd31bf75
ryuyama <ryu.yamada.private@gmail.com>
2015-04-01 23:00:17 +0900

    Eliminate repetition in tests (solves exercies 3.1)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,27 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  # Called before each tests.
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_title_is "Home"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_title_is "Help"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_title_is "About"
+    assert_select "title", "About | #{@base_title}"
   end
-
-  private
-
-    def assert_title_is(name)
-      assert_select "title", "#{name} | Ruby on Rails Tutorial Sample App"
-    end
 end
