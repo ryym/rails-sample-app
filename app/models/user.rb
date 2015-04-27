3e0b6085f13d969b68775ec36172b4d6879861e8
ryuyama <ryu.yamada.private@gmail.com>
2015-04-27 21:59:07 +0900

    Add a secure password to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ActiveRecord::Base
     length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
+  validates :password,
+    length: { minimum: 6 }
 
+  has_secure_password
   before_save { self.email = email.downcase }
 end
