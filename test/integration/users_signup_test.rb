a7d13d807b6f9c4f51bfe28cbea2c37537f09928
ryym <ryym.64@gmail.com>
2015-06-28 01:30:58 +0900

    Log in automatically after signing up

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,6 +26,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/show'
+    assert is_logged_in?, 'The user has singed up logs in automatically.'
   end
 
 end
