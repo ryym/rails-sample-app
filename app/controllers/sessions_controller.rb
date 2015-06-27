8519389278b58a01eb1b33abd44bb69be397536d
ryym <ryym.64@gmail.com>
2015-06-28 00:22:30 +0900

    Add a login functionality using sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    session = params[:session]
+    user = User.find_by(email: session[:email].downcase)
+    if user && user.authenticate(session[:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = "invalid email/password combination"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
