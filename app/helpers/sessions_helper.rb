b1aa7ed64b261c3b25b1e139e585f7b547ade941
ryym <ryym.64@gmail.com>
2015-06-28 23:53:00 +0900

    Implements Log out functionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   # Returns the current logged-in user (if any).
   def current_user
     # Use 'find_by()' instead of 'find()' not to raise exception
