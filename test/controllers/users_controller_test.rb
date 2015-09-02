7b65af121e1dfae15f5cf1843104665c4602ff5c
ryym <ryym.64@gmail.com>
2015-09-02 22:36:03 +0900

    Show all users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,4 +43,9 @@ class UsersControllerTest < ActionController::TestCase
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
 end
