9bbd58c82f516d3ca04b7a8696f4f45c8e7d5aad
ryym <ryym.64@gmail.com>
2015-09-05 18:33:14 +0900

    Register the first seed user as an admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
-def create_user(name, email)
-  User.create!(
+def create_user(name, email, &block)
+  user = User.new(
     name: name,
     email: email,
     password: 'password',
     password_confirmation: 'password'
   )
+  block.call user if block_given?
+  user.save
 end
 
-create_user 'Example User', 'example@railstutorial.org'
+create_user 'Example User', 'example@railstutorial.org' do |user|
+  user.admin = true
+end
 
 99.times do |n|
   name  = Faker::Name.name
