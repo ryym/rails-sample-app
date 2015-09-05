e21607102ab103338f2177c4fe2c7db61d8ef498
ryym <ryym.64@gmail.com>
2015-09-05 20:49:09 +0900

    Add tests for destroying users

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:michael)
+    @user      = users(:michael)
+    @admin     = users(:michael)
+    @non_admin = users(:archer)
   end
 
   test 'index incuding pagination' do
@@ -14,5 +16,27 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
+  end
+
+  test 'index as admin' do
+    log_in_as @admin
+    get users_path
+
+    assert_template 'users/index'
+    User.paginate(page: 1) do |user|
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+
+  test 'index as non-admin' do
+    log_in_as @non_admin
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
