ebc3fe51b3392e7f0621e5fb1e2180a46e0efd56
ryym <ryym.64@gmail.com>
2015-09-09 23:53:53 +0900

    User: Generate activation token before creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   has_secure_password
   before_save :downcase_email
+  before_create :create_activation_digest
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name,
@@ -51,5 +52,11 @@ class User < ActiveRecord::Base
     # Converts email to all lower-case.
     def downcase_email
       self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 end
