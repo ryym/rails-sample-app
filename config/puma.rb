826a8a732bccde12e3c3e02b8129ddd9a339dced
ryym <ryym.64@gmail.com>
2015-09-09 22:57:43 +0900

    Fix typo on the config file for puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,13 @@
-worders Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
 
-rackup DefaultRackup
-port ENV['PORT'] || 3000
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  AciveRecord::Base.establish_connection
+  ActiveRecord::Base.establish_connection
 end
