94d2e7eca040c805dba9e7c3decbebc9260bb809
ryym <ryym.64@gmail.com>
2015-11-16 21:50:49 +0900

    Associate users and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ActiveRecord::Base
-  has_many :microposts, dependent: :destroy
+  has_many :microposts,
+    dependent: :destroy
+
+  has_many :active_relationships,
+    class_name: 'Relationship',
+    foreign_key: 'follower_id',
+    dependent: :destroy
+
+  has_many :passive_relationships,
+    class_name: 'Relationship',
+    foreign_key: 'followed_id',
+    dependent: :destroy
+
+  has_many :following,
+    through: :active_relationships,
+    source: :followed # Collect following users using the 'followed_id'.
+
+  has_many :followers,
+    through: :passive_relationships
 
   attr_accessor :remember_token, :activation_token, :reset_token
   has_secure_password
